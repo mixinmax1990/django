22a070051198ce7ba86ea9190908c964533b0c20
Danny Hahn <Danny@Dannys-iMac.local>
2019-10-05 09:37:50 +0200

    updates

--- tasks/settings.py
+++ tasks/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tasks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'tasks/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'tasks.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "djangodb",
+        "USER": "mixinmax",
+        "PASSWORD": "seether1990",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
